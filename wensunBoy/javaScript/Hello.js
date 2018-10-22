d71ac6bfd30db34651d50bb43b1eb7bb35088d20
Wenbinxiao1996 <598719574@qq.com>
2018-10-22 18:08:32 +0800

    10.22练习与作业

---
+++ wensunBoy/javaScript/Hello.js
@@ -0,0 +1,8 @@
+//alert("Hello JS");
+var name=prompt("请输入您的姓名：")//按F12，在Chorome的控制器查看变量值。
+console.log(name);
+var a="欢迎来到";
+console.log(name);
+var b="JS的世界";
+console.log(name);
+alert(name+',你好,'+a+b);
