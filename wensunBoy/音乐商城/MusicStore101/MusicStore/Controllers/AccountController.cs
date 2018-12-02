0ac2baee10edde7e397581a59acd1e8c41fb85fc
Wenbinxiao1996 <598719574@qq.com>
2018-12-02 11:02:59 +0800

    12.2音乐商城(制作到专辑显示)

---
+++ wensunBoy/音乐商城/MusicStore101/MusicStore/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using MusicStoreEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private static readonly EntityDbContext _context = new EntityDbContext();
+        // GET: Account
+        /// <summary>
+        /// 用户信息 登入和注册
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult login(Guid id)
+        {
+            var detail = _context.Albums.Find(id);
+            return View(detail);
+        }
+        //public ActionResult Register()
+        //{
+        //    var detail = _context.Albums.Find(id);
+        //    return View(detail);
+        //}
+    }
+}
