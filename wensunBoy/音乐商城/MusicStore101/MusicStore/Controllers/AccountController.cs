859e062823a75741e6e3b3bf784a472582c08bb5
Wenbinxiao1996 <598719574@qq.com>
2018-12-06 11:43:51 +0800

    能注册 正常登入 可以修改密码

--- wensunBoy/音乐商城/MusicStore101/MusicStore/Controllers/AccountController.cs
+++ wensunBoy/音乐商城/MusicStore101/MusicStore/Controllers/AccountController.cs
@@ -21,22 +21,56 @@ namespace MusicStore.Controllers
         /// <param name="id"></param>
         /// <returns></returns>
 
-        public ActionResult Register(string returnUrl=null)
+        public ActionResult Register()
         {
-            if (string.IsNullOrEmpty(returnUrl))
-                ViewBag.ReturnUrl = Url.Action("Login", "Account");
-            else
-                ViewBag.ReturnUrl = returnUrl;
             return View();
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Register(RegisterViewModel model)
         {
-            //用户的保存 Person ApplicationUser
-            return View();
+            if (ModelState.IsValid)
+            {
+                var person = new Person()
+                {
+                    FirstName = model.FullName.Substring(0, 1),
+                    LastName = model.FullName.Substring(1, model.FullName.Length - 1),
+                    Name = model.FullName,
+                    CredentialsCode = "",
+                    Birthday = DateTime.Now,
+                    Sex = true,
+                    MobileNumber = "17687724994",
+                    Email = model.Email,
+                    TelephoneNumber = "17687724994",
+                    Description = "",
+                    CreateDateTime = DateTime.Now,
+                    UpdateTime = DateTime.Now,
+                    InquiryPassword = "未设置",
+                };
+                var user = new ApplicationUser()
+                {
+                    UserName = model.UserName,
+                    FirstName = model.FullName.Substring(0, 1),
+                    LastName = model.FullName.Substring(1, model.FullName.Length - 1),
+                    ChineseFullName = model.FullName,
+                    MobileNumber = "17687724994",
+                    Email = model.Email,
+                    Person = person,
+                };
 
+                //是否要验证Email
+
+                var idManager = new IdentityManager();
+                idManager.CreateUser(user, model.PassWord);
+                idManager.AddUserToRole(user.Id, "RegisterUser");
+
+                return Content("<script>alert('恭喜注册成功!');location.href='" + Url.Action("login", "Account") + "'</script>");
+            }
+
+            return View();
         }
+
         /// <summary>
         /// 登入方法
         /// </summary>
@@ -97,9 +131,65 @@ namespace MusicStore.Controllers
                     var identity = userManage.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
                     return Redirect(returnUrl);
                 }
+                
             }
+            return View();
+        }
+        //注销方法
+        public ActionResult LoginOut()
+        {
+            Session.Remove("loginStatus");
+            Session.Remove("LoginUserSessionModel");
+            return RedirectToAction("index", "Home");
+        }
+
+        //修改密码
+      
+        public ActionResult ChangePassWord()
+        {
+            //用户先登入了才能修改密码
+            if (Session["LoginUserSessionModel"] == null)
+                return RedirectToAction("Login");
 
             return View();
+        }
+        [HttpPost]
+        public ActionResult ChangePassWord(ChangePassWordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //输入合法，进行修改密码
+                bool changePwdSuccessed;
+                try
+                {
+                    var userManage =
+                        new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new EntityDbContext()));
+                    var userName = (Session["LoginUserSessionModel"] as LoginUserSeesionModel).User.UserName;
+                    //判断原密码是否输入正确
+                    var user = userManage.Find(userName, model.PassWord);
+                    if (user == null)
+                    {
+                        ModelState.AddModelError("", "原密码不正确");
+                        return View(model);
+                    }
+                    else
+                    {
+                        //修改密码
+                        changePwdSuccessed = userManage.ChangePassword(user.Id, model.PassWord, model.NewPassWord)
+                            .Succeeded;
+                        if (changePwdSuccessed)
+                            return Content("<script>alert('恭喜修改密码成功!');location.href='" + Url.Action("index", "home") +
+                                           "'</script>");
+                        else
+                            ModelState.AddModelError("", "修改密码失败，请重试");
+                    }
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "修改密码失败，请重试");
+                }
+            }
+            return View(model);
         }
     }
 }
