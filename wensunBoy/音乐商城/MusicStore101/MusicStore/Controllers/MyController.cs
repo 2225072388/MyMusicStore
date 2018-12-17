744e861c8b32c7acb127a8c9ed73eb0cb50821fa
Wenbinxiao1996 <598719574@qq.com>
2018-12-17 17:58:45 +0800

    12.17 昨天欠下的添加头像 今天地图定位

--- wensunBoy/音乐商城/MusicStore101/MusicStore/Controllers/MyController.cs
+++ wensunBoy/音乐商城/MusicStore101/MusicStore/Controllers/MyController.cs
@@ -2,6 +2,7 @@
 using MusicStoreEntity;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,26 +11,79 @@ namespace MusicStore.Controllers
 {
     public class MyController : Controller
     {
-         private static readonly EntityDbContext _context = new EntityDbContext();
+        private static readonly EntityDbContext _context = new EntityDbContext();
         // GET: My
-        //public ActionResult Add()
-        //{
-        //    return View();
-        //}
+        //修改个人信息
         public ActionResult Index()
         {
-            EntityDbContext _context = new EntityDbContext();
+            //EntityDbContext _context = new EntityDbContext();
+
+            //var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
+            //var orders = Session["Order"] as Order;
+            ////创造新的Order对象
+            //var order = new Order()
+            //{
+            //    AddressPerson = person.Name,
+            //    MobilNumber = person.MobileNumber,
+            //    Person = _context.Persons.Find(person.ID),
+            //};
+            //return View(order);
+            if (Session["LoginUserSessionModel"] == null)
+                return RedirectToAction("login", "Account", new { returnUrl = Url.Action("Index", "My") });
 
             var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
-            var orders = Session["Order"] as Order;
-            //创造新的Order对象
-            var order = new Order()
+
+            var myVM = new MyViewModel()
             {
-                AddressPerson = person.Name,
-                MobilNumber = person.MobileNumber,
-                Person = _context.Persons.Find(person.ID),
+                Name = person.Name,
+                Address = person.Address,
+                MobliNumber = person.MobileNumber,
+
             };
-            return View(order);
+
+            ViewBag.AvardaUrl = person.Avarda;
+
+            return View(myVM);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(MyViewModel model)
+        {
+            if (Session["LoginUserSessionModel"] == null)
+                return RedirectToAction("login", "Account", new { returnUrl = Url.Action("Index", "My") });
+
+            var person = _context.Persons.Find((Session["LoginUserSessionModel"] as LoginUserSessionModel).Person.ID);
+
+            //用户原来的头像
+            var oldAvarda = person.Avarda;
+
+            if (ModelState.IsValid)
+            {
+                //保存头像
+                if (model.Avarda != null)
+                {
+                    var uploadDir = "~/Upload/Avarda/";
+                    //取后缀名
+                    var fileLastName = model.Avarda.FileName.Substring(model.Avarda.FileName.LastIndexOf(".") + 1,
+                        (model.Avarda.FileName.Length - model.Avarda.FileName.LastIndexOf(".") - 1));
+                    var imagePath = Path.Combine(Server.MapPath(uploadDir), person.ID + "." + fileLastName);//将网站虚拟路径转化成真实的物理路径
+                    model.Avarda.SaveAs(imagePath);
+                    oldAvarda = "~/Upload/Avarda/" + person.ID + "." + fileLastName;
+                }
+                //保存个人信息
+
+                person.MobileNumber = model.MobliNumber;
+                person.Address = model.Address;
+                person.Name = model.Name;
+                person.FirstName = person.Name.Substring(0, 1);
+                person.LastName = person.Name.Substring(1, person.Name.Length - 1);
+                person.Avarda = oldAvarda;
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            ViewBag.AvardaUrl = oldAvarda;
+            return View();
         }
     }
 }
