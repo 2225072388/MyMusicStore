f09872500062e310acc02d90919f9b4770d2d763
Wenbinxiao1996 <598719574@qq.com>
2018-12-21 11:43:32 +0800

    12.21 修复头像不能显示的bug

--- wensunBoy/音乐商城/MusicStore101/MusicStore/Controllers/MyController.cs
+++ wensunBoy/音乐商城/MusicStore101/MusicStore/Controllers/MyController.cs
@@ -12,22 +12,10 @@ namespace MusicStore.Controllers
     public class MyController : Controller
     {
         private static readonly EntityDbContext _context = new EntityDbContext();
-        // GET: My
-        //修改个人信息
+
+        // 修改个人信息
         public ActionResult Index()
         {
-            //EntityDbContext _context = new EntityDbContext();
-
-            //var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
-            //var orders = Session["Order"] as Order;
-            ////创造新的Order对象
-            //var order = new Order()
-            //{
-            //    AddressPerson = person.Name,
-            //    MobilNumber = person.MobileNumber,
-            //    Person = _context.Persons.Find(person.ID),
-            //};
-            //return View(order);
             if (Session["LoginUserSessionModel"] == null)
                 return RedirectToAction("login", "Account", new { returnUrl = Url.Action("Index", "My") });
 
@@ -37,14 +25,14 @@ namespace MusicStore.Controllers
             {
                 Name = person.Name,
                 Address = person.Address,
-                MobliNumber = person.MobileNumber,
-
+                MobilNumber = person.MobileNumber
             };
 
             ViewBag.AvardaUrl = person.Avarda;
 
             return View(myVM);
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Index(MyViewModel model)
@@ -53,7 +41,6 @@ namespace MusicStore.Controllers
                 return RedirectToAction("login", "Account", new { returnUrl = Url.Action("Index", "My") });
 
             var person = _context.Persons.Find((Session["LoginUserSessionModel"] as LoginUserSessionModel).Person.ID);
-
             //用户原来的头像
             var oldAvarda = person.Avarda;
 
@@ -66,22 +53,24 @@ namespace MusicStore.Controllers
                     //取后缀名
                     var fileLastName = model.Avarda.FileName.Substring(model.Avarda.FileName.LastIndexOf(".") + 1,
                         (model.Avarda.FileName.Length - model.Avarda.FileName.LastIndexOf(".") - 1));
-                    var imagePath = Path.Combine(Server.MapPath(uploadDir), person.ID + "." + fileLastName);//将网站虚拟路径转化成真实的物理路径
+                    var imagePath = Path.Combine(Server.MapPath(uploadDir), person.ID + "." + fileLastName);  //将网站虚拟路径转化为真实的物理路径
                     model.Avarda.SaveAs(imagePath);
-                    oldAvarda = "~/Upload/Avarda/" + person.ID + "." + fileLastName;
+                    oldAvarda = "/Upload/Avarda/" + person.ID + "." + fileLastName;
                 }
-                //保存个人信息
 
-                person.MobileNumber = model.MobliNumber;
+                //保存个人信息
+                person.MobileNumber = model.MobilNumber;
                 person.Address = model.Address;
                 person.Name = model.Name;
                 person.FirstName = person.Name.Substring(0, 1);
                 person.LastName = person.Name.Substring(1, person.Name.Length - 1);
                 person.Avarda = oldAvarda;
+
                 _context.SaveChanges();
 
                 return RedirectToAction("Index");
             }
+
             ViewBag.AvardaUrl = oldAvarda;
             return View();
         }
--- wensunBoy/音乐商城/MusicStore101/MusicStore/ViewModels/MyViewModel.cs
+++ wensunBoy/音乐商城/MusicStore101/MusicStore/ViewModels/MyViewModel.cs
@@ -5,20 +5,23 @@ using System.Linq;
 using System.Web;
 
 namespace MusicStore.ViewModels
-{
+{   /// <summary>
+    /// 用户信息视图模型
+    /// </summary>
     public class MyViewModel
     {
-        /// <summary>
-        /// 用户信息视图模型
-        /// </summary>
-        [Display(Name="收件人姓名")]
-        [Required(ErrorMessage ="收件人姓名不能为空")]
+        [Display(Name = "收件人姓名")]
+        [Required(ErrorMessage = "收件人姓名不能为空")]
         public string Name { get; set; }
+
         [Display(Name = "收货地址")]
         [Required(ErrorMessage = "收货地址不能为空")]
         public string Address { get; set; }
+
         [Display(Name = "收件人手机")]
-        public string MobliNumber { get; set; }
+        [Required(ErrorMessage = "收件人手机不能为空")]
+        public string MobilNumber { get; set; }
+
         [Display(Name = "头像")]
         [DataType(DataType.Upload)]
         public HttpPostedFileBase Avarda { get; set; }
