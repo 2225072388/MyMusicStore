b348e2bff630500cd3d783e718a70e8560cb84db
luyusheng7 <2278814961@qq.com>
2019-01-05 00:04:19 +0800

    完成点赞

--- luyusheng/05MusicStore1.1/MusicStore101/MusicStoreEntity/EntityDbContext.cs
+++ luyusheng/05MusicStore1.1/MusicStore101/MusicStoreEntity/EntityDbContext.cs
@@ -31,13 +31,14 @@ namespace MusicStoreEntity
         #region 音乐商店实体
         public IDbSet<Genre> Genres { get; set; }
         public IDbSet<Artist> Artists { get; set; }
-        public IDbSet<Album> Albums { get; set; }
-        public IDbSet<LikeReply> LikeReply { get; set; }
+        public IDbSet<Album> Albums { get; set; }  
 
         public IDbSet<Cart> Carts { get; set; }
         public IDbSet<Order> Orders { get; set; }
         public IDbSet<OrderDetail> OrderDetails { get; set; }
+        public object PersonOrders { get; set; }
         public IDbSet<Reply> Replys { get; set; }
+        public IDbSet<LikeReply> LikeReply { get; set; }
 
         #endregion
     }
